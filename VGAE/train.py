5b39ee86e5303340ccac02bccd6feb1249d1e0ab
chengpei-wu <1683168521@qq.com>
2024-01-28 10:58:10 +0800

    modify vgae

--- VGAE/train.py
+++ VGAE/train.py
@@ -1,76 +1,123 @@
-import torch
-import torch.nn.functional as F
-from dataset import SyntheticDataset
-from dgl.dataloading import GraphDataLoader
-from torch import optim
-from torch.utils.data import random_split
-
-from model import VGAE
-
-
-def kl_divergence(z, mu, log_sigma):
-    return (
-        0.5
-        / z.size(0)
-        * (1 + 2 * log_sigma - mu**2 - torch.exp(log_sigma) ** 2).sum(1).mean()
-    )
-
-
-def train(
-    device: torch.device,
-    model: torch.nn.Module,
-    dataset,
-    max_epoch: int,
-    batch_size: int,
-):
-    train_loader = GraphDataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        pin_memory=True,
-        num_workers=4,
-    )
-
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
-    model.to(device)
-
-    for epoch in range(max_epoch):
-        model.train()
-        total_loss = 0
-        for batch, batched_graph in enumerate(train_loader):
-            batched_graph = batched_graph.to(device)
-            h = batched_graph.ndata.pop("deg_feat")
-            logits = model(batched_graph, h, device)
-            adj = batched_graph.adj().to_dense()
-            loss = F.binary_cross_entropy(logits.view(-1), adj.view(-1))
-            divergence = kl_divergence(logits, model.mu, model.log_sigma)
-            loss = loss - divergence
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        scheduler.step()
-        print(f"Epoch {epoch}--Loss: {total_loss / (batch + 1):.4f}")
-
-
-if __name__ == "__main__":
-    # dataset
-    nets = SyntheticDataset(
-        network_type="er", average_degree=(3, 10), num_nodes=100, num_graphs=10
-    )
-    train_size = int(0.9 * len(nets))
-    val_size = len(nets) - train_size
-    train_dataset, val_dataset = random_split(nets, [train_size, val_size])
-
-    # train
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-
-    model = VGAE(
-        in_feats=1,
-        hidden_feats=16,
-        out_feats=2,
-    ).to(device)
-    train(device=device, model=model, dataset=train_dataset, max_epoch=5, batch_size=1)
-    torch.save(model, "./model")
+# import argparse
+#
+# import dgl
+# import dgl.function as fn
+# import torch
+# import torch.nn as nn
+# from dgl import AddSelfLoop
+# from dgl.data import CiteseerGraphDataset
+# from dgl.data import CoraGraphDataset
+# from dgl.data import PubmedGraphDataset
+# from torch import optim
+#
+# from model import VGAE
+# import torch.nn.functional as F
+#
+#
+# class DotProductPredictor(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def forward(self, graph, h):
+#         with graph.local_scope():
+#             graph.ndata["h"] = h
+#             graph.apply_edges(fn.u_dot_v("h", "h", "score"))
+#             return graph.edata["score"]
+#
+#
+# def kl_divergence(z, mu, log_sigma):
+#     return (
+#         0.5
+#         / z.size(0)
+#         * (1 + 2 * log_sigma - mu**2 - torch.exp(log_sigma) ** 2).sum(1).mean()
+#     )
+#
+#
+# def train(
+#     g,
+#     features,
+#     masks,
+#     model,
+#     max_epoch,
+# ):
+#     train_mask = masks[0]
+#     val_mask = masks[1]
+#     optimizer = optim.Adam(model.parameters(), lr=0.01)
+#     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
+#     model.to(device)
+#
+#     for epoch in range(max_epoch):
+#         model.train()
+#         logits = model(g, features)
+#         adj = g.adj().to_dense()
+#         loss = F.binary_cross_entropy(logits.view(-1), adj.view(-1))
+#         divergence = kl_divergence(logits, model.mu, model.log_sigma)
+#         loss = loss - divergence
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         scheduler.step()
+#         print(f"Epoch {epoch}--Loss: {loss.item():.4f}")
+#
+#
+# # Implementation based on Dropping all edges during training GNN.
+#
+# if __name__ == "__main__":
+#     args_list = [
+#         ("--dataset", "pubmed"),
+#         ("--datatype", "float"),
+#         ("--hid_size", 16),
+#         ("--lr", 1e-2),
+#         ("--max_epoch", 200),
+#     ]
+#
+#     # argument configuration
+#     parser = argparse.ArgumentParser()
+#
+#     for arg_name, default in args_list:
+#         parser.add_argument(arg_name, default=default)
+#     args = parser.parse_args()
+#
+#     # load and preprocess dataset
+#     if args.dataset == "cora":
+#         if args.self_loop:
+#             data = CoraGraphDataset(transform=AddSelfLoop())
+#         else:
+#             data = CoraGraphDataset()
+#
+#     elif args.dataset == "citeseer":
+#         if args.self_loop:
+#             data = CiteseerGraphDataset(transform=AddSelfLoop())
+#         else:
+#             data = CiteseerGraphDataset()
+#     elif args.dataset == "pubmed":
+#         if args.self_loop:
+#             data = PubmedGraphDataset(transform=AddSelfLoop())
+#         else:
+#             data = PubmedGraphDataset()
+#     else:
+#         raise ValueError("Unknown dataset: {}".format(args.dataset))
+#
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#
+#     g = data[0]
+#     g = g.int().to(device)
+#
+#     features = g.ndata["feat"]
+#     labels = g.ndata["label"]
+#     masks = g.ndata["train_mask"], g.ndata["val_mask"], g.ndata["test_mask"]
+#
+#     # create GCN model
+#     in_size = features.shape[1]
+#     out_size = data.num_classes
+#
+#     model = VGAE(in_size, args.hid_size, out_size).to(device)
+#
+#     # convert model and graph to bfloat16 if needed
+#     if args.datatype == "bfloat16":
+#         g = dgl.to_bfloat16(g)
+#         features = features.to(dtype=torch.bfloat16)
+#         model = model.to(dtype=torch.bfloat16)
+#
+#     # model training
+#     train(g, features, labels, masks, model, lr=args.lr, max_epoch=args.max_epoch)
